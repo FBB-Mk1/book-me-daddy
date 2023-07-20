c8a0c7e03f79618a8bbe7bb46e09586d465a43b4
fabin <fabiobatista@gmail.com>
2023-07-20 10:41:41 -0300

    Nicer UI

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -19,13 +19,13 @@ export default function Books() {
 
   const deleteBook = api.book.delete.useMutation({
     onSuccess: (data) => {
-      setBookList((prev) => prev.filter((item) => item.id !== data.id))
-    }
+      setBookList((prev) => prev.filter((item) => item.id !== data.id));
+    },
   });
-
 
   return (
     <>
+      
       <div>
         {modalToggle && (
           <BookModal
@@ -34,18 +34,21 @@ export default function Books() {
           />
         )}
       </div>
-      <div className="mx-auto my-12 max-w-3xl">
-        <button
-          className="rounded-sm border px-2"
-          onClick={() => setModalToggle(true)}
-        >
-          Inserir Livro
-        </button>
-        <div className="flex">
+      <div className="m-auto h-full w-full flex-col">
+        <div className="my-3 grid">
+          <button
+            className="mx-auto place-self-center rounded-sm bg-slate-200 px-2"
+            onClick={() => setModalToggle(true)}
+          >
+            Inserir Livro
+          </button>
+        </div>
+        {deleteBook.isLoading && <div className="absolute inset-0 flex items-center justify-center bg-black/50"></div>}
+        <div className="flex flex-wrap">
           {bookList.map((book) => (
             <div
               key={book.id}
-              className="m-2 flex h-60 w-40 flex-col bg-red-600 text-white"
+              className="mx-2 my-2 flex h-60 w-40 w-40 flex-col bg-red-600 text-white"
             >
               <span className="h-[20%] self-center text-xl">{book.title}</span>
               <span className="self-end px-2 text-sm">{book.author}</span>
@@ -53,7 +56,7 @@ export default function Books() {
               <button
                 key={book.id}
                 className="mx-2 self-end border px-2"
-                onClick={() => deleteBook.mutate({id: book.id})}
+                onClick={() => deleteBook.mutate({ id: book.id })}
               >
                 Delete
               </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,41 @@
 import Head from "next/head";
 import Header from "~/components/Header";
-import SideBar from "~/components/SideBar";
-import Books from "~/components/Books";
 
+import Books from "~/components/Books";
+import { signIn, useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: sessionData } = useSession();
+
   return (
     <>
       <Head>
         <title>Book-me</title>
         <meta name="description" content="Book me daddy" />
         <link rel="icon" href="/favicon.ico" />
-        
       </Head>
-      <main className="flex h-screen w-screen flex-col">
-        <Header />
-        <div className="flex h-full w-full bg-slate-100">
-          <SideBar />
-          <Books />
-        </div>
+      <main className="flex h-screen w-screen flex-col content-center">
+        {sessionData?.user ? (
+          <div className="flex h-full w-full flex-col bg-slate-100">
+            <div>
+              <Header />
+            </div>
+            <div className="flex h-full w-full bg-slate-100">
+              
+              <Books />
+            </div>
+          </div>
+        ) : (
+          <div className="flex h-full w-full content-center justify-center">
+            <button
+              className="h-40 w-40 self-center rounded-full bg-slate-200 text-3xl text-red-900 text-white"
+              onClick={() => void signIn()}
+            >
+              SignIn
+            </button>
+          </div>
+        )}
       </main>
     </>
   );
 }
-
-
-
-
